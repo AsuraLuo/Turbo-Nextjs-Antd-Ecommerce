08ded1aa235940edfc2e845ee5ec2ad8e847cd0b
caesar <caesar.luo@silksoftware.com>
2023-02-14 16:39:19 +0800

    chore: express server http2

--- apps/storefront/server.ts
+++ apps/storefront/server.ts
@@ -1,18 +1,21 @@
-// import fs from 'fs'
+import fs from 'fs'
 import next from 'next'
 import express from 'express'
 import { parse } from 'url'
-// import { createServer } from 'spdy'
+import { createServer, ServerOptions } from 'spdy'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-// const options: any = {
-//   key: fs.readFileSync('keys/nextjs-key.pem'),
-//   cert: fs.readFileSync('keys/nextjs-cert.pem')
-// }
+const options: ServerOptions = {
+  key: fs.readFileSync('keys/nextjs-key.pem'),
+  cert: fs.readFileSync('keys/nextjs-cert.pem'),
+  spdy: {
+    protocols: ['h2']
+  }
+}
 
 app.prepare().then(() => {
   const server = express()
@@ -23,13 +26,13 @@ app.prepare().then(() => {
   })
 
   // start the HTTP/2 server with koa
-  // const httpServer: any = createServer(options, server.callback())
+  const httpServer: any = createServer(options, server)
 
-  server.listen(port, () => {
-    // if (error) {
-    //   console.error(error)
-    //   return process.exit(1)
-    // }
+  httpServer.listen(port, (error: any) => {
+    if (error) {
+      console.error(error)
+      return process.exit(1)
+    }
 
     return console.warn(
       `> Server listening at http://localhost:${port} as ${
