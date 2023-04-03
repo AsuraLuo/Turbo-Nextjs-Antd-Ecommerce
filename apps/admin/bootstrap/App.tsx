12a34ac05e4becbfab6d1d0cd0cc23960fef9bd5
caesar <caesar.luo@silksoftware.com>
2023-04-03 08:21:56 +0800

    feat: qauntity

--- apps/admin/bootstrap/App.tsx
+++ apps/admin/bootstrap/App.tsx
@@ -9,17 +9,13 @@ import { store } from '@store/index'
 import { Provider as ReduxProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import { OConfigProvider, OCssBaseline, OForm, OButton, IQuantityStep } from '@ocloud/ui'
+import { OConfigProvider, OCssBaseline } from '@ocloud/ui'
 
 import AppShell from '@components/AppShell'
 import HttpProvider from '@components/HttpProvider'
 import LocaleProvider from '@components/LocaleProvider'
 
 const App = () => {
-  const onFinish = (values: any) => {
-    console.info(values)
-  }
-
   return (
     <ReduxProvider store={store}>
       <HttpProvider>
@@ -30,12 +26,7 @@ const App = () => {
                 <OConfigProvider theme={{}} prefixCls={ThemeConf.namespace}>
                   <OCssBaseline />
                   <AppShell />
-                  <div style={{ width: '500px', margin: '10rem auto' }}>
-                    <OForm onFinish={onFinish}>
-                      <IQuantityStep />
-                      <OButton htmlType="submit">Submit</OButton>
-                    </OForm>
-                  </div>
+                  <div style={{ width: '500px', margin: '10rem auto' }} />
                 </OConfigProvider>
               </ThemeProvider>
             </StyleProvider>
---
+++ packages/ui/IUploadFile/iUploadFile.tsx
@@ -0,0 +1,12 @@
+import OModal from '../OModal'
+import OUpload from '../OUpload'
+
+const IUploadFile = () => {
+  return (
+    <OModal open>
+      <OUpload />
+    </OModal>
+  )
+}
+
+export default IUploadFile
