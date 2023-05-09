9e144e98091f0706c32454f55cbc1290aa6bc566
caesar <caesar.luo@silksoftware.com>
2023-05-09 08:11:12 +0800

    feat: update

--- packages/ui/components/ORadio/index.ts
+++ packages/ui/components/ORadio/index.ts
@@ -1,15 +1,14 @@
-import { Radio, RadioProps } from 'antd'
+import { Radio } from 'antd'
 
 import InternalRadio from './radio'
 
 const { Button, Group } = Radio
 
 type InternalRadioType = typeof InternalRadio
-type CompoundedComponent = React.ForwardRefExoticComponent<RadioProps> &
-  InternalRadioType & {
-    Button: typeof Button
-    Group: typeof Group
-  }
+type CompoundedComponent = InternalRadioType & {
+  Button: typeof Button
+  Group: typeof Group
+}
 
 const ORadio = InternalRadio as CompoundedComponent
 ORadio.Button = Radio.Button
